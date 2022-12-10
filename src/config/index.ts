e0829089a16e9f26c7e50f2edb80e3073ecf1d0d
kasun gamage <kasun.thaksala@gmail.com>
2022-12-10 09:57:29 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 /* Local mongo url */
-export const MONGO_LOCAL_URL = 'mongodb://localhost:27017/online_food_delivery';
+export const MONGO_LOCAL_URL = '';
 /* Cloud mongo url */
-export const MONGO_URL = 'mongodb+srv://syborg2290:6h9s70RioBQgxvMr@foodsdelivery.m8wgg.mongodb.net/online_food_delivery?retryWrites=true&w=majority';
+export const MONGO_URL = '';
 
-export const APP_SECRET = 'JSDH746353_SDFJHS6#$#%#$%DFHS_112DSFJH5837FDSJ';
+export const APP_SECRET = '';
 
 export const PORT = process.env.PORT || 8000;
